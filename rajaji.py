04bd13cca18ba3c4871dbff0859002556cd762ca
hwkwkwk1k <dcplayz511@gmail.com>
2025-01-10 23:57:43 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7808154142:AAFKhag-6xJobkAOqrn351BLauVhMOeUXSM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("BGMIDDOS1245")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7279164468")
+
+
+
+
+  
